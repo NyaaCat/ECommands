7905156a724c0eb61b919a99097599fae3fce0ac
lori3f6 <lori3f6@gmail.com>
2022-01-23 18:59:09 +0800

    function: show pending payment

--- src/main/java/cat/nyaa/ecommands/commands/PayCommand.java
+++ src/main/java/cat/nyaa/ecommands/commands/PayCommand.java
@@ -1,16 +1,13 @@
 package cat.nyaa.ecommands.commands;
 
 import cat.nyaa.ecommands.SpigotLoader;
-import cat.nyaa.ecommands.lang.MainLang;
 import cat.nyaa.ecommands.utils.Payment;
-import cat.nyaa.ecore.EconomyCore;
 import land.melon.lab.simplelanguageloader.utils.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
 import java.util.*;
@@ -25,7 +22,7 @@ public class PayCommand implements CommandExecutor {
     }
 
     // Usage:
-    // /pay <amount|confirm|cancel> [player...]
+    // /pay <amount|confirm|cancel|show> [player...]
     @Override
     public boolean onCommand(@Nonnull CommandSender commandSender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
         if (!(commandSender instanceof Player player)) {
@@ -33,41 +30,67 @@ public class PayCommand implements CommandExecutor {
             return true;
         }
         if (args.length == 1) {
-            if (args[0].equalsIgnoreCase("help")) {
-                player.sendMessage(pluginInstance.getMainLang().payCommand.help.produce());
-            } else if (args[0].equalsIgnoreCase("confirm")) {
-                if (!waitingPayments.containsKey(player.getUniqueId())) {
-                    player.sendMessage(pluginInstance.getMainLang().payCommand.noWaitingForConfirmTransfer.produce());
+            switch (args[0].toLowerCase()){
+                case "help" ->{
+                    player.sendMessage(pluginInstance.getMainLang().payCommand.help.produce());
                     return true;
-                } else {
-                    var result = waitingPayments.get(player.getUniqueId()).confirm();
-                    if (result.isSuccess()) {
-                        var receivers = result.getReceipt().getReceiver().stream()
-                                .map(uuid -> Bukkit.getOfflinePlayer(uuid).getName()).collect(Collectors.joining(", "));
-                        player.sendMessage(pluginInstance.getMainLang().payCommand.transferSuccess.produce(
-                                Pair.of("totallyCost", result.getReceipt().getCostTotally()),
-                                Pair.of("amount", result.getReceipt().getAmountPerTransaction()),
-                                Pair.of("receiver", receivers),
-                                Pair.of("serviceFee", result.getReceipt().getFeeTotally()),
-                                Pair.of("serviceFeePercent", result.getReceipt().getFeeRate() * 100),
-                                Pair.of("receiptId", Long.toHexString(result.getReceipt().getId()))
-                        ));
+                }
+                case "confirm" -> {
+                    if (!waitingPayments.containsKey(player.getUniqueId())) {
+                        player.sendMessage(pluginInstance.getMainLang().payCommand.noWaitingForConfirmTransfer.produce());
                     } else {
-                        player.sendMessage(pluginInstance.getMainLang().payCommand.transferFailed.produce());
+                        var result = waitingPayments.get(player.getUniqueId()).confirm();
+                        if (result.isSuccess()) {
+                            var receivers = result.getReceipt().getReceiver().stream()
+                                    .map(uuid -> Bukkit.getOfflinePlayer(uuid).getName()).collect(Collectors.joining(", "));
+                            player.sendMessage(pluginInstance.getMainLang().payCommand.transferSuccess.produce(
+                                    Pair.of("totallyCost", result.getReceipt().getCostTotally()),
+                                    Pair.of("amount", result.getReceipt().getAmountPerTransaction()),
+                                    Pair.of("receiver", receivers),
+                                    Pair.of("serviceFee", result.getReceipt().getFeeTotally()),
+                                    Pair.of("serviceFeePercent", result.getReceipt().getFeeRate() * 100),
+                                    Pair.of("receiptId", Long.toHexString(result.getReceipt().getId()))
+                            ));
+                        } else {
+                            player.sendMessage(pluginInstance.getMainLang().payCommand.transferFailed.produce());
+                        }
                     }
-
+                    return true;
                 }
-            } else if (args[0].equalsIgnoreCase("cancel")) {
-                if (!waitingPayments.containsKey(player.getUniqueId())) {
-                    player.sendMessage(pluginInstance.getMainLang().payCommand.noWaitingForConfirmTransfer.produce());
-                } else {
-                    waitingPayments.remove(player.getUniqueId());
-                    player.sendMessage(pluginInstance.getMainLang().payCommand.transferCancelled.produce());
+                case "cancel" -> {
+                    if (!waitingPayments.containsKey(player.getUniqueId())) {
+                        player.sendMessage(pluginInstance.getMainLang().payCommand.noWaitingForConfirmTransfer.produce());
+                    } else {
+                        waitingPayments.remove(player.getUniqueId());
+                        player.sendMessage(pluginInstance.getMainLang().payCommand.transferCancelled.produce());
+                    }
+                    return true;
                 }
-                return true;
-            } else {
-                return false;
+                case "show" -> {
+                    if (!waitingPayments.containsKey(player.getUniqueId())) {
+                        player.sendMessage(pluginInstance.getMainLang().payCommand.noWaitingForConfirmTransfer.produce());
+                    }else{
+                        var payment = waitingPayments.get(player.getUniqueId());
+                        var receivers = payment.to().stream().map(uuid -> Bukkit.getOfflinePlayer(uuid).getName())
+                                .collect(Collectors.joining(", "));
+                        var amount = payment.amount() / (1+pluginInstance.getEconomyCore().getTransferFeeRate());
+                        var totallyCost = payment.amount() * payment.to().size();
+                        player.sendMessage(pluginInstance.getMainLang().payCommand.transferConfirm.produce(
+                                Pair.of("amount", amount),
+                                Pair.of("receivers", receivers),
+                                Pair.of("totallyCost", totallyCost),
+                                Pair.of("serviceFee", payment.amount() - amount),
+                                Pair.of("serviceFeePercent", pluginInstance.getEconomyCore().getTransferFeeRate() * 100),
+                                Pair.of("balanceAfterTransfer", pluginInstance.getEconomyCore().getPlayerBalance(player.getUniqueId()) - totallyCost)
+                        ));
+                    }
+                    return true;
+                }
+                default -> {
+                    return false;
+                }
             }
+
         } else if (args.length >= 2) {
             double amount;
             try {
@@ -104,6 +127,7 @@ public class PayCommand implements CommandExecutor {
             var totallyCost = amount * (1 + pluginInstance.getEconomyCore().getTransferFeeRate() * targets.size());
             var receivers = targets.stream().map(uuid -> Bukkit.getOfflinePlayer(uuid).getName())
                     .collect(Collectors.joining(", "));
+
             if (pluginInstance.getEconomyCore().getPlayerBalance(player.getUniqueId()) < totallyCost) {
                 player.sendMessage(pluginInstance.getMainLang().payCommand.insufficientBalance.produce(
                         Pair.of("totallyCost", totallyCost),
@@ -125,8 +149,9 @@ public class PayCommand implements CommandExecutor {
                     Pair.of("balanceAfterTransfer", pluginInstance.getEconomyCore().getPlayerBalance(player.getUniqueId()) - totallyCost)
             ));
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
 }
