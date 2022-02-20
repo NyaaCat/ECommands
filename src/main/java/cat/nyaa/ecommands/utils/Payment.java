09e7e6b64c3f8b162f7285f49c14517b9a145546
lori3f6 <lori3f6@gmail.com>
2022-02-20 17:43:42 +0800

    fix: outdated interface call

--- src/main/java/cat/nyaa/ecommands/utils/Payment.java
+++ src/main/java/cat/nyaa/ecommands/utils/Payment.java
@@ -1,14 +1,14 @@
 package cat.nyaa.ecommands.utils;
 
 import cat.nyaa.ecore.EconomyCore;
-import cat.nyaa.ecore.TradeResult;
+import cat.nyaa.ecore.TransactionResult;
 
 import java.util.List;
 import java.util.UUID;
 
 public record Payment(UUID from, List<UUID> to, double amount,
                       EconomyCore economyCoreInstance) {
-    public TradeResult confirm() {
+    public TransactionResult confirm() {
         return economyCoreInstance.playerTransferToMultiple(from, to, amount);
     }
 }
